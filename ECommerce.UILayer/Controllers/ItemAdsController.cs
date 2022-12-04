93dce534b91c0c4dbd37cda054ecbbcb3530c700
iremsmr <iremsamur129@gmail.com>
2022-12-04 19:38:59 +0300

    Customer main page is added

--- ECommerce.UILayer/Controllers/ItemAdsController.cs
+++ ECommerce.UILayer/Controllers/ItemAdsController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerce.BusinessLayer.Abstract;
+using ECommerce.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ECommerce.UILayer.Controllers
 {
     public class ItemAdsController : Controller
     {
+        private readonly IItemService _itemService;
+
+        public ItemAdsController(IItemService itemService)
+        {
+            _itemService = itemService;
+        }
+
         public IActionResult GetAllItemAds()
         {
-            return View();
+            List<Item> items = new List<Item>();
+            var values = _itemService.TGetItemWithImage();
+            var count = values.Count();
+            for (int i = 0;i < count;i++)
+            {
+
+                if (i < 3)
+                {
+                    items.Add(values[i]);
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return View(items);
+            
         }
     }
 }
